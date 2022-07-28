01f99aedbfe0bb0cae2bc88808375a1251aca10b
saidjalolqodirov <saidjalolqodirov1@gmail.com>
2022-07-28 21:35:20 +0500

    commit 2

--- src/main/java/uz/ml/delivering_rest/entity/entity/Region.java
+++ src/main/java/uz/ml/delivering_rest/entity/entity/Region.java
@@ -5,6 +5,9 @@ import uz.ml.delivering_rest.entity.Auditable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,5 +20,6 @@ public class Region extends Auditable {
     private String name;
     private String otherFields;
 
-
+    @ManyToMany(mappedBy = "regions",fetch = FetchType.EAGER)
+    private List<Carrier> carrier;
 }
